b9fbdc475eb50a4c2ab9fe46f7b5f116b4d24c5e
davut kok <davut84@yhoo.com>
2020-10-13 10:25:26 +0300

    errorHandling

--- src/actions/PokemonActions.js
+++ src/actions/PokemonActions.js
@@ -3,32 +3,44 @@ export const FETCH_SELECTED_POKEMON = "FETCH_SELECTED_POKEMON";
 export const ADD_NEW_POKEMONS = "ADD_NEW_POKEMONS";
 export const fetchPokemons = () => {
   return async (dispatch) => {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon`);
-    const data = await response.json();
-    dispatch({
-      type: FETCH_POKEMONS,
-      payload: data.results,
-      nextUrl: data.next,
-    });
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon`);
+      const data = await response.json();
+      dispatch({
+        type: FETCH_POKEMONS,
+        payload: data.results,
+        nextUrl: data.next,
+      });
+    } catch (err) {
+      throw err;
+    }
   };
 };
 
 export const fetchSelectedPokemon = (id) => {
   return async (dispatch) => {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    const data = await response.json();
-    dispatch({ type: FETCH_SELECTED_POKEMON, payload: data });
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+      const data = await response.json();
+      dispatch({ type: FETCH_SELECTED_POKEMON, payload: data });
+    } catch (err) {
+      throw err;
+    }
   };
 };
 
 export const addNewPokemons = (url) => {
   return async (dispatch) => {
-    const response = await fetch(`${url}`);
-    const data = await response.json();
-    dispatch({
-      type: ADD_NEW_POKEMONS,
-      payload: data.results,
-      nextUrl: data.next,
-    });
+    try {
+      const response = await fetch(`${url}`);
+      const data = await response.json();
+      dispatch({
+        type: ADD_NEW_POKEMONS,
+        payload: data.results,
+        nextUrl: data.next,
+      });
+    } catch (err) {
+      throw err;
+    }
   };
 };
--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -2,21 +2,27 @@ import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Button, Card } from "semantic-ui-react";
+import { Button, Card, Segment } from "semantic-ui-react";
 import { addNewPokemons, fetchPokemons } from "../actions/PokemonActions";
 import PokemonListItem from "./PokemonListItem";
 import LoadingComponent from "./LoadingComponent";
 
 export default function PokemonList() {
   const [isLoading, setIsLoading] = useState(false);
+  const [errorState, setErrorState] = useState(false);
   const dispatch = useDispatch();
   const pokemonList = useSelector((state) => state.pokemons.pokemonList);
   const nextUrl = useSelector((state) => state.pokemons.nextUrl);
   useEffect(() => {
     const loadPokemons = async () => {
       setIsLoading(true);
-      await dispatch(fetchPokemons());
-      setIsLoading(false);
+      try {
+        await dispatch(fetchPokemons());
+      } catch (error) {
+        setErrorState(true);
+      } finally {
+        setIsLoading(false);
+      }
     };
     loadPokemons();
   }, [dispatch]);
@@ -25,6 +31,13 @@ export default function PokemonList() {
     return <LoadingComponent />;
   }
 
+  if (errorState) {
+    return (
+      <Segment textAlign="center">
+        <h1>Something went wrong while loading pokemons</h1>
+      </Segment>
+    );
+  }
   return (
     <div>
       <Card.Group className="main">
